31af88e93ab6b9f43bb5ede8905dd3e28abf98f2
Priyanshi Patel <priyanshi21.patel@gmail.com>
2024-04-29 18:48:29 +0530

    Employee CRUD app

---
+++ EmployeeProject/Models/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeProject.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Employee TagNumber")]
+        public string EmployeeTagNumber { get; set; }
+
+        [Required(ErrorMessage ="Please Enter First Name")]
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Department { get; set; }
+        public int? Age { get; set; }
+        public string? Designation { get; set; }
+    }   
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EmployeeProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
