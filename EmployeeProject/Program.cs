53f1e36850222c9604a376eb318816b3259797b0
PatelPriyanshi <51357151+PatelPriyanshi@users.noreply.github.com>
2024-05-01 10:56:01 +0530

    Update Program.cs

--- EmployeeProject/Program.cs
+++ EmployeeProject/Program.cs
@@ -1,7 +1,13 @@
+using EmployeeProject.DBEntities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<EmployeeContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
